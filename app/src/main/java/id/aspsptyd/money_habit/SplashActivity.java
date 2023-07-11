345d44522a5c533d87684c5ce17462e7bf234a59
aspsptyd <devadiroot@gmail.com>
2023-07-11 12:07:02 +0700

    Direct activity splashscreen ke home screen

--- app/src/main/java/id/aspsptyd/money_habit/SplashActivity.java
+++ app/src/main/java/id/aspsptyd/money_habit/SplashActivity.java
@@ -2,13 +2,34 @@ package id.aspsptyd.money_habit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
 
+    Handler holdOn;
+
+    TextView tvSplashApp;
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        tvSplashApp = (TextView) findViewById(R.id.txWelcome);
+
+        tvSplashApp.setText(getString(R.string.welcome_splash) + " " + getString(R.string.version_app));
+
+        holdOn = new Handler();
+
+        holdOn.postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+            finish();
+        }, 2500);
+
     }
 }
